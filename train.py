33ac2c7831aa34fe99b245c68c20e847a5260da5
Adam <am.rivero13@gmail.com>
2017-03-14 16:55:42 +0100

    file readers thread number to 32

--- train.py
+++ train.py
@@ -246,9 +246,9 @@ def main():
         raise
 
     threads = tf.train.start_queue_runners(sess=sess, coord=coord)
-    reader.start_threads(sess, n_threads=8) # will use 3 for train and 1 for validation queues
+    reader.start_threads(sess, n_threads=32) # will use 3 for train and 1 for validation queues
 
-    time.sleep(20)
+    time.sleep(300)
 
     step = None
     try:
